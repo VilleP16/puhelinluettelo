e4ba02f086e859be59bfd82e64e70c186b5be5c4
villep <pekvil16@gmail.com>
2022-09-22 14:44:48 +0300

    henkilöiden listaus sivulle

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Cristiano Ronaldo'}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) =>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange = {handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <PersonsList persons = {persons}/>
+      <h2>Numbers</h2>
+      ...
+      <div>debug: {newName}</div>
+      
+    </div>
+  )
+
+}
+
+const PersonsList  = ({persons}) =>{
+  console.log(persons)
+  return(
+    <div>
+      {persons.map((person) =>{
+        return <Person name = {person.name} key = {person.name} />
+      })}
+
+    </div>
+
+  )
+}
+const Person = (props) => {
+  return(
+    <p>{props.name}</p>
+  )
+}
+
+
+export default App
